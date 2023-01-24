e06c3c7803d3c682db7036c0b6a8b94546d26b64
cinos81 <cinos81@gmail.com>
2023-01-24 20:25:04 +0900

    feat: 애니메이션 컨트롤 실습

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import gsap from 'gsap'
 import './index.css'
 
-const timeline = gsap.timeline()
-Array.from(document.getElementsByName('box')).forEach((elem, index) => {
-  timeline.to(elem, {
-    duration: 1, x: 200, scale: 0.1 * index
-  })
+declare const nav: HTMLDivElement
+
+const tween = gsap.to(document.getElementsByName('box'), {
+  duration: 4,
+  x: () => nav.offsetWidth, // animate by the px width of the nav
+  xPercent: -100, // offset by the width of the box
+  rotation: 360,
+  ease: "none",
+  paused: true
 })
+
+declare const play: HTMLButtonElement
+play.onclick = () => tween.play()
+
+declare const pause: HTMLButtonElement
+pause.onclick = () => tween.pause()
+
+declare const resume: HTMLButtonElement
+resume.onclick = () => tween.resume()
+
+declare const reverse: HTMLButtonElement
+reverse.onclick = () => tween.reverse()
+
+declare const restart: HTMLButtonElement
+restart.onclick = () => tween.restart()
