a8cb9065ce18967de1326818addf8ff2332bf85a
cinos81 <cinos81@gmail.com>
2023-01-24 18:55:11 +0900

    feat: stagger 실습

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import gsap from 'gsap'
 import './index.css'
 
-declare const elem: HTMLDivElement
-
-gsap.to(elem, { duration: 1, x: 100, y: 100, rotation: 45 })
+gsap.from(document.getElementsByName('box'), {   duration: 2,
+  scale: 0.5,
+  opacity: 0,
+  delay: 0.5,
+  stagger: 0.2,
+  ease: "elastic",
+  force3D: true
+})
